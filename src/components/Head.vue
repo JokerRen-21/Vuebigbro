dece49acc7bb738cdb41abebb0daec4e7093b63d
JokerRen328 <=>
2023-10-11 15:29:12 +0800

    mfw trying to god damn place em tgt with v-bind

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -5,6 +5,17 @@
     <div class=" font-thin underline p-4 text-xl ">
         This is how you are suppose to make Vue JS as a whole
     </div>
+<div class="flex flex-row">
+    <div>this right here is only just a fraction of vue js that i am gonna check</div>
+    <div>
+        <div>
+            <img :src="LogitechG" alt="">
+        </div>
+        <div>
+            <img :src="" alt="">
+        </div>
+    </div>
+</div>
 </template>
 <style scoped>
 
