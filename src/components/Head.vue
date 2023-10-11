b80981563760619ede3ada7cee74cb523a300a7c
JokerRen328 <=>
2023-10-11 15:54:40 +0800

    where is vue?

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -5,15 +5,13 @@
     <div class=" font-thin underline p-4 text-xl ">
         This is how you are suppose to make Vue JS as a whole
     </div>
-<div class="flex flex-row">
-    <div>this right here is only just a fraction of vue js that i am gonna check</div>
-    <div>
-        <div>
-            <img :src="LogitechG" alt="">
-        </div>
-        <div>
-            <img :src="" alt="">
-        </div>
+<div class="flex flex-row items-center justify-center">
+    <div class="text-right grid grid-rows-2 grid-flow-col w-7/12 gap-y-2  items-center">
+    <div class=" row-span-2"> <span class="underline"> this right here is only just a fraction of vue js that i am gonna check:</span>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae ex blanditiis ducimus velit delectus obcaecati harum perspiciatis, vitae totam sint! Deleniti odio fuga nostrum, in sit odit quia porro maiores!
+    </div>
+    <div class="flex gap-5 items-center flex-row justify-center"><img src="https://i.pinimg.com/originals/03/d6/a4/03d6a40f8be9403b3d20949d84bbb1d1.png" class="w-4/12" alt="">Logitech G</div>
+    <div class="flex gap-5 items-center flex-row justify-center"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Lenovo_Global_Corporate_Logo.png/2560px-Lenovo_Global_Corporate_Logo.png" class="w-4/12" alt="">Lenovo G</div>
     </div>
 </div>
 </template>
