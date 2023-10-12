0f6c2a3f925492193b7f95603ecce6bc81e7aa48
JokerRen-21 <132533360+JokerRen-21@users.noreply.github.com>
2023-10-12 11:47:22 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Bod from './components/Bod.vue'
 import Head from './components/Head.vue'
 import Foot from './components/Foot.vue'
+  //might as well question yourself if you don't even know this basic stuff 'me'
 </script>
 
 <template>
