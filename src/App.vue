ee7dd5f77c0a5c6a6dffaafa7431e28669667a26
JokerRen328 <=>
2023-10-16 14:45:41 +0800

    Merge branch 'main' of https://github.com/JokerRen-21/Vuebigbro

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Bod from './components/Bod.vue'
 import Head from './components/Head.vue'
 import Foot from './components/Foot.vue'
+import head2 from './components/head2.vue';
   //might as well question yourself if you don't even know this basic stuff 'me'
 </script>
 
@@ -9,6 +10,7 @@ import Foot from './components/Foot.vue'
   <div class="flex flex-col justify-center items-center">
     <div class=" w-9/12">
   <Head/>
+  <head2/>
   <Bod/>
   <Foot/>
 </div>
---
+++ src/components/head2.vue
@@ -0,0 +1,18 @@
+<template>
+
+<div class="flex flex-col items-center justify-center">
+<div class="flex flex-row items-center justify-center">
+    <div class="text-right grid grid-rows-2 grid-flow-col w-7/12 gap-y-2  items-center">
+    <div class=" row-span-2"> <span class="underline"> this right here is only just a fraction of vue js that i am gonna check:</span>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae ex blanditiis ducimus velit delectus obcaecati harum perspiciatis, vitae totam sint! Deleniti odio fuga nostrum, in sit odit quia porro maiores!
+    </div>
+    <div class="flex gap-5 items-center flex-row justify-center"><img src="https://i.pinimg.com/originals/03/d6/a4/03d6a40f8be9403b3d20949d84bbb1d1.png" class="w-4/12" alt="">Logitech G</div>
+    <div class="flex gap-5 items-center flex-row justify-center"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Lenovo_Global_Corporate_Logo.png/2560px-Lenovo_Global_Corporate_Logo.png" class="w-4/12" alt="">Lenovo G</div>
+    </div>
+</div>
+<div class="p-3 text-base font-[300]">Hi hello this is Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint voluptates culpa veritatis sequi itaque officia delectus! Dolorem voluptate veniam debitis, autem consequuntur rem repellendus esse, nam praesentium, quam vitae nisi?
+Laudantium, recusandae voluptate, nostrum esse pariatur optio veniam temporibus eaque magnam reiciendis ea error asperiores quos non nulla tempore sapiente illum nesciunt vitae voluptatibus! Exercitationem quisquam nostrum qui commodi soluta.
+Animi ratione excepturi voluptate accusamus harum veritatis illo iste quod velit similique, in, a odit molestiae ipsum enim maiores. Magnam, magni error atque distinctio nulla earum delectus rerum et aspernatur.</div>
+</div>
+    
+</template>
